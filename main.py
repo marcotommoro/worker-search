f7b1de90a7b953270a52ed874ada292d0161ae54
Marco Moroni <marcotommoro@skipper.local>
2022-07-02 15:17:17 +0200

    feat: add valueserp

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import json
 import os
+
+import requests as req
+from dotenv import load_dotenv
+
+load_dotenv()
 
 try:
     from urllib import request
@@ -13,9 +19,10 @@ except ImportError:
 
 user = os.environ.get("OC_USER")
 passwd = os.environ.get("OC_PASSWORD")
+api_key = os.environ.get("API_KEY")
 
-if not user or not passwd:
-    print("No user or password found")
+if not user or not passwd or not api_key:
+    print("No user or password or api_key found")
     exit(1)
 
 oc = owncloud.Client("https://cloud.marcotommoro.duckdns.org/")
@@ -31,23 +38,43 @@ if os.path.exists("./urls.txt"):
 if not os.path.exists("./docs") or not os.path.isdir("./docs"):
     os.mkdir("./docs")
 
+
+# set up the request parameters
+params = {
+    "api_key": api_key,
+    "q": "",
+    "location": "Italy",
+    "gl": "it",
+    "hl": "it",
+    "google_domain": "google.it",
+    "num": "10",
+}
+
+
 for comune in comuni.comuni:
     codice_nome = f'{comune["codice"]}_{comune["nome"]}'
     query = f'comune "{comune["nome"]}" "azzardo" ordinanza filetype:pdf | filetype:doc | filetype:docx | filetype:rtf'
 
+    print(codice_nome)
     try:
         oc.mkdir(f"documenti/{codice_nome}")
     except owncloud.ResponseError as e:
         print(e)
         pass
 
-    print(query)
+    params["q"] = query
 
-    for url in search(query, num=10, pause=2):
-        print(url)
+    api_result = req.get("https://api.valueserp.com/search", params)
+    print("remaining: ", api_result.json()["request_info"]["topup_credits_remaining"])
+
+    for result in api_result.json()["organic_results"]:
+        url = result["link"]
+
         if url in url_list:
+            print(url, "already downloaded.")
             continue
 
+        print("fetching...", url)
         url_list.append(url)
 
         try:
